3d13a09695ddda4d742e4b3c5896cdf863c9ed19
Louis Jean ARNAUD <bones.you.spooky@gmail.com>
2019-03-20 16:50:18 +0100

    portfolio - init

---
+++ assets/js/app.js
@@ -0,0 +1,54 @@
+$(".button_profil").click(() =>
+{
+	$("#first_page").toggleClass("hidden")
+	$("#profil").toggleClass("hidden")
+})
+
+$(".button_knowledge").click(() =>
+{
+	$("#first_page").toggleClass("hidden")
+	$("#knowledge").toggleClass("hidden")
+})
+
+$(".button_projects_list").click(() =>
+{
+	$("#first_page").toggleClass("hidden")
+	$("#projects_list").toggleClass("hidden")
+})
+
+$(".button_contact").click(() =>
+{
+	$("#first_page").toggleClass("hidden")
+	$("#contact").toggleClass("hidden")
+})
+
+$(".project_blorum").click(() =>
+{
+	$(".blorum").toggleClass("hidden")
+})
+
+$(".project_waip").click(() =>
+{
+	$(".waip").toggleClass("hidden")
+})
+
+$(".project_math").click(() =>
+{
+	$(".math").toggleClass("hidden")
+})
+
+$(".project_install_OS").click(() =>
+{
+	$(".install_OS").toggleClass("hidden")
+})
+
+$(".project_System_Support").click(() =>
+{
+	$(".system_Support").toggleClass("hidden")
+})
+
+
+$(".desc").click(() =>
+{
+	$(".description").toggleClass("hidden")
+})
